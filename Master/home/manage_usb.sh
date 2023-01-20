e5a425f8d80e5252565f24fb90307cc522d046bf
Vitaliy Didenko <dvit009@gmail.com>
2023-01-20 21:32:52 +0300

    manage-usb

--- Master/home/manage_usb.sh
+++ Master/home/manage_usb.sh
@@ -8,29 +8,37 @@
 ### Роли настраиваюся в правилах OpenHab                                             ###
 ########################################################################################
 
-USB_1="-vid 0a89 -pid 0025 -usb-port 1-7.1" #Aktiv Rutoken lite
-USB_2="-vid 0529 -pid 0620 -usb-port 1-7.3" #Aladdin Token JC Etoken Велес/Велком
-USB_3="-vid 0529 -pid 0620 -usb-port 1-7.4" #Aladdin Token JC URALSIB ИП
-USB_4="-vid 0529 -pid 0001 -usb-port 1-7.2" #AKS HASP 2.17
+USB_1="-vid 0a89 -pid 0025 -usb-port 1-7.1" #Aktiv Rutoken lite FNS IP/Velkom
+USB_2="-vid 0a89 -pid 0025 -usb-port 5-2"   #Aktiv Rutoken lite FNS Veles KAU
+USB_3="-vid 0a89 -pid 0030 -usb-port 1-7.4" #Aktiv Rutoken ESP URALSIB IP/Velkom/Veles KAU
+
+USB_4="-vid 0a89 -pid 0030 -usb-port 1-7.3" #Aktiv Rutoken ESP URALSIB IP/Velkom DVN
+
+#USB_5="-vid 0529 -pid 0001 -usb-port 1-7.2" #AKS HASP 2.17
+
 
 #Отключаем все устройства
 usbsrv -unshare $USB_1
 usbsrv -unshare $USB_2
 usbsrv -unshare $USB_3
 usbsrv -unshare $USB_4
+#usbsrv -unshare $USB_5
 
 #Подключаем устройства доступные всем
 usbsrv -share $USB_1
+usbsrv -share $USB_2
+usbsrv -share $USB_3
 
 #Подключаем устройства доступные для роли Бухгалтер
 if [[ $1 == "2"  ]] || [[ $1 == "3" ]]
 then
-    usbsrv -share $USB_2
-    usbsrv -share $USB_3
+    usbsrv -share $USB_4
+    #usbsrv -share $USB_3
+    #usbsrv -share $USB_5
 fi
 
 #Подключаем оставшиеся устройства доступные для роли Мастер
-if [[ $1 == "3" ]]
-then
-    usbsrv -share $USB_4
-fi
+#if [[ $1 == "3" ]]
+#then
+    #usbsrv -share $USB_5
+#fi
